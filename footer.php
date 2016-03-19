8df94bd626527338410b31ebd966b4d6e65220ea
BalalRaza <balal.raza@gmail.com>
2016-03-19 13:01:44 +0530

    Added files via upload

---
+++ footer.php
@@ -0,0 +1,8 @@
+		<footer class="navbar navbar-fixed-bottom">
+			<div class="footer">
+				<center><h3 class="token">Made by <span class="names">Md Balal Raza</span> and <span class="names">Mrinmoy Das</span> of CSE 3rd year, as on March, 2016</h3></center>
+			</div>
+		</footer>
+		
+	</body>
+</html>
